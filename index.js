e0afb1a45ffd1cc645e1f852b8c760a954735999
snoor_madih <snoor.madih@gmail.com>
2021-05-14 20:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+let arrayCopy = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+        for (let i = 0; i < arrayCopy.length; i++) {
+          callback(arrayCopy[i]) }
+           console.log("collection", collection)
+      return collection
     },
 
     map: function() {
